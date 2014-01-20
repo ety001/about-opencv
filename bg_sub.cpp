cb3cea13c9c949659653dc8070900a8c2325ff16
ety001 <ety001@domyself.me>
2014-01-20 23:51:30 +0800

    bg_sub can read from video camara directly

--- bg_sub.cpp
+++ bg_sub.cpp
@@ -27,19 +27,16 @@ int keyboard; //input from keyboard
 /** Function Headers */
 void help();
 void processVideo();
-void processImages(char* firstFrameFilename);
 
 void help()
 {
     cout
     << "--------------------------------------------------------------------------" << endl
     << "This program shows how to use background subtraction methods provided by "  << endl
-    << " OpenCV. You can process both videos (-v) and images (-img)."             << endl
+    << " OpenCV. You can process videos camara."             << endl
                                                                                     << endl
     << "Usage:"                                                                     << endl
-    << "./bs {-vid <video filename>|-img <image filename>}"                         << endl
-    << "for example: ./bs -vid video.avi"                                           << endl
-    << "or: ./bs -img /data/images/1.png"                                           << endl
+    << "for example: ./bs"                                           << endl
     << "--------------------------------------------------------------------------" << endl
     << endl;
 }
@@ -52,13 +49,6 @@ int main(int argc, char* argv[])
     //print help information
     help();
 
-    //check for the input parameter correctness
-    if(argc != 3) {
-        cerr <<"Incorret input list" << endl;
-        cerr <<"exiting..." << endl;
-        return EXIT_FAILURE;
-    }
-
     //create GUI windows
     namedWindow("Frame");
     namedWindow("FG Mask MOG");
@@ -68,20 +58,8 @@ int main(int argc, char* argv[])
     pMOG = new BackgroundSubtractorMOG(); //MOG approach
     pMOG2 = new BackgroundSubtractorMOG2(); //MOG2 approach
 
-    if(strcmp(argv[1], "-v") == 0) {
-        //input data coming from a video
-        processVideo();
-    }
-    else if(strcmp(argv[1], "-img") == 0) {
-        //input data coming from a sequence of images
-        processImages(argv[2]);
-    }
-    else {
-        //error in reading input parameters
-        cerr <<"Please, check the input parameters." << endl;
-        cerr <<"Exiting..." << endl;
-        return EXIT_FAILURE;
-    }
+    processVideo();
+
     //destroy GUI windows
     destroyAllWindows();
     return EXIT_SUCCESS;
@@ -95,7 +73,7 @@ void processVideo() {
     VideoCapture capture(0);
     if(!capture.isOpened()){
         //error in opening the video input
-        cerr << "Unable to open video file: " << videoFilename << endl;
+        cerr << "Unable to open video  camara" << endl;
         exit(EXIT_FAILURE);
     }
     //read input data. ESC or 'q' for quitting
@@ -126,61 +104,4 @@ void processVideo() {
     }
     //delete capture object
     capture.release();
-}
-
-/**
- * @function processImages
- */
-void processImages(char* fistFrameFilename) {
-    //read the first file of the sequence
-    frame = imread(fistFrameFilename);
-    if(!frame.data){
-        //error in opening the first image
-        cerr << "Unable to open first image frame: " << fistFrameFilename << endl;
-        exit(EXIT_FAILURE);
-    }
-    //current image filename
-    string fn(fistFrameFilename);
-    //read input data. ESC or 'q' for quitting
-    while( (char)keyboard != 'q' && (char)keyboard != 27 ){
-        //update the background model
-        pMOG->operator()(frame, fgMaskMOG);
-        pMOG2->operator()(frame, fgMaskMOG2);
-        //get the frame number and write it on the current frame
-        size_t index = fn.find_last_of("/");
-        if(index == string::npos) {
-            index = fn.find_last_of("\\");
-        }
-        size_t index2 = fn.find_last_of(".");
-        string prefix = fn.substr(0,index+1);
-        string suffix = fn.substr(index2);
-        string frameNumberString = fn.substr(index+1, index2-index-1);
-        istringstream iss(frameNumberString);
-        int frameNumber = 0;
-        iss >> frameNumber;
-        rectangle(frame, cv::Point(10, 2), cv::Point(100,20),
-                  cv::Scalar(255,255,255), -1);
-        putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
-                FONT_HERSHEY_SIMPLEX, 0.5 , cv::Scalar(0,0,0));
-        //show the current frame and the fg masks
-        imshow("Frame", frame);
-        imshow("FG Mask MOG", fgMaskMOG);
-        imshow("FG Mask MOG 2", fgMaskMOG2);
-        //get the input from the keyboard
-        keyboard = waitKey( 30 );
-        //search for the next image in the sequence
-        ostringstream oss;
-        oss << (frameNumber + 1);
-        string nextFrameNumberString = oss.str();
-        string nextFrameFilename = prefix + nextFrameNumberString + suffix;
-        //read the next frame
-        frame = imread(nextFrameFilename);
-        if(!frame.data){
-            //error in opening the next image in the sequence
-            cerr << "Unable to open image frame: " << nextFrameFilename << endl;
-            exit(EXIT_FAILURE);
-        }
-        //update the path of the current frame
-        fn.assign(nextFrameFilename);
-    }
 }
